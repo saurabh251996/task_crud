30885593d9fa00a37602281bafe7b39caee47409
saurabh251996 <61679299+saurabh251996@users.noreply.github.com>
2020-03-05 15:30:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+require("./database/db");
+const express=require("express");
+const bodyparse=require("body-parser");
+const route=require("./routes/route");
+const mongoose=require("mongoose");
+
+const app=express();
+app.use(bodyparse.json());
+app.use(bodyparse.urlencoded({extended:true}));
+
+app.use("/user",route);
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+})
